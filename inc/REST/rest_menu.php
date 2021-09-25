bcb3ef0430427b28a0161a665b0d6366cb0b0d41
antlogist <antlogist@gmail.com>
2021-09-25 15:47:19 +0300

    Footer menu route added

--- inc/REST/rest_menu.php
+++ inc/REST/rest_menu.php
@@ -10,8 +10,17 @@ function chip_menu() {
     'methods' => WP_REST_SERVER:: READABLE,
     'callback' => 'get_menu',
   ) );
+  
+  register_rest_route( 'menus/v1', 'footer-menu', array(
+    'methods' => WP_REST_SERVER:: READABLE,
+    'callback' => 'get_footer_menu',
+  ) );
 }
 
 function get_menu() {
   return wp_get_nav_menu_items('main');
+}
+
+function get_footer_menu() {
+  return wp_get_nav_menu_items('footer');
 }
