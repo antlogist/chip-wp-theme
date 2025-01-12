739ef2691fe06251ebf8966b9eb9df773e80e888
antlogist <antlogist@yandex.ru>
2025-01-12 09:20:52 +0300

    Filter site menu info in REST API endpoint #14
    Implement filtering logic in REST API endpoint to sanitize and limit site menu information, enhancing security and preventing unauthorized access.

--- inc/REST/rest_menu.php
+++ inc/REST/rest_menu.php
@@ -1,26 +1,43 @@
 <?php
+if (! defined("ABSPATH")) {
+  header("Location: /");
+  exit;
+}
 
-// Exit if accessed directly
-if ( ! defined ('ABSPATH') ) {
-    exit;
-} 
-
-function chip_menu() {
-  register_rest_route( 'menus/v1', 'menu', array(
-    'methods' => WP_REST_SERVER:: READABLE,
+function chip_menu()
+{
+  register_rest_route('menus/v1', 'menu', array(
+    'methods' => WP_REST_SERVER::READABLE,
     'callback' => 'get_menu',
-  ) );
-  
-  register_rest_route( 'menus/v1', 'footer-menu', array(
-    'methods' => WP_REST_SERVER:: READABLE,
+  ));
+
+  register_rest_route('menus/v1', 'footer-menu', array(
+    'methods' => WP_REST_SERVER::READABLE,
     'callback' => 'get_footer_menu',
-  ) );
+  ));
 }
 
-function get_menu() {
-  return wp_get_nav_menu_items('main');
+function get_menu()
+{
+  $menu_items = wp_get_nav_menu_items('main');
+  $filtered_items = filter_nav($menu_items);
+  return $filtered_items;
 }
 
-function get_footer_menu() {
-  return wp_get_nav_menu_items('footer');
+function get_footer_menu()
+{
+  $menu_items = wp_get_nav_menu_items('footer');
+  $filtered_items = filter_nav($menu_items);
+  return $filtered_items;
+}
+
+function filter_nav($objects)
+{
+  $filteredObjects = array_reduce($objects, function ($carry, $item) {
+    $newItem = ["url" => $item->url, "title" => $item->title];
+    $carry[] = $newItem;
+    return $carry;
+  }, []);
+
+  return $filteredObjects;
 }
