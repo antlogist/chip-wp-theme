311ac3804bb6d7ee5b0a80f24c7ead209fad1a1b
antlogist <antlogist@yandex.ru>
2025-01-14 09:56:46 +0300

    Modified post and page loop

--- single.php
+++ single.php
@@ -15,7 +15,15 @@ get_template_part('template-parts/pagebuttons', 'basic');
     <div class="row g-0">
       <div class="col-md-9">
         <div class="post-wrapper">
-          <?php the_content(); ?>
+          <?php
+          if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+              <h2><?php the_title(); ?></h2>
+              <p class="small">Posted by <b><?php the_author_posts_link() ?></b> on <?= get_the_date('d.m.y') ?> in <b><?= get_the_category_list(', ') ?></b></p>
+              <p><?php the_content(); ?></p>
+          <?php
+            endwhile;
+          endif; ?>
         </div>
       </div>
       <!--Recent posts-->
