20ae6302a6804d1a75d44e7edd3d972c62e31f05
antlogist <antlogist@yandex.ru>
2025-01-17 09:53:47 +0300

    Add pre tag to dd function
    Enhance dd function with pre tag for improved syntax highlighting and easier debugging.

--- functions.php
+++ functions.php
@@ -64,7 +64,9 @@ if (!function_exists('dd')) {
   function dd(...$vars)
   {
     foreach ($vars as $var) {
+      echo '<pre>';
       var_dump($var);
+      echo '</pre>';
     }
     die(1);
   }
