2bdcbca6b7073b43e4032b245b1e02ced0010021
antlogist <antlogist@yandex.ru>
2025-01-19 20:02:53 +0300

    Enqueue custom CSS styles on registration page #37
    Load custom CSS stylesheet on registration page, enhancing visual appeal and user experience.

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,22 +1,33 @@
 <?php
 
 //Exit if accessed directly
-if ( ! defined ('ABSPATH') ) {
+if (! defined('ABSPATH')) {
   exit;
 }
 
-function chip_styles_and_scripts() {
+function chip_styles_and_scripts()
+{
   //libs CSS
-  wp_enqueue_style('overlap-lib-css', get_theme_file_uri('dist/css/libs.min.css'));  
-    
+  wp_enqueue_style('overlap-lib-css', get_theme_file_uri('dist/css/libs.min.css'));
+
   //custom CSS
   wp_enqueue_style('overlap-app-css', get_theme_file_uri('dist/css/all.min.css'), NULL, microtime());
-  
+
   //custom js
   wp_enqueue_script('overlap-app-js', get_theme_file_uri('dist/js/all.min.js'), null, microtime(), true);
 }
 
-function chip_customizer_script() {
+function chip_customizer_script()
+{
   //custom js
-  wp_enqueue_script('chip-customizer-js', get_theme_file_uri('dist/js/theme-customize.min.js'), array( 'jquery','customize-preview' ), microtime(), true);
+  wp_enqueue_script('chip-customizer-js', get_theme_file_uri('dist/js/theme-customize.min.js'), array('jquery', 'customize-preview'), microtime(), true);
+}
+
+function login_styles()
+{
+  //libs CSS
+  wp_enqueue_style('overlap-lib-css', get_theme_file_uri('dist/css/libs.min.css'));
+
+  //custom CSS
+  wp_enqueue_style('overlap-app-css', get_theme_file_uri('dist/css/all.min.css'), NULL, microtime());
 }
